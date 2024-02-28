0f91eb43104d5257e753e291c993318849aa4bce
NunoGitHub <nunoaapp22@hotmail.com>
2024-02-28 21:03:39 +0000

    mat-card, form field  and text area

--- mean-stack/src/app/posts/post-create/post-create.component.ts
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -3,7 +3,24 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent{
+
+  newPost:any ="NO CONTENT";
+  enteredValue:any = "";
+
+  onAddPost(postInput:HTMLTextAreaElement){
+   // alert("Post added!")
+   console.dir(postInput)
+   //this.newPost= 'The user\'s new post';
+   this.newPost= postInput.value;
+  }
+
+
+  onAddPostEventBinding(){
+    this.newPost= this.enteredValue;
+    alert(this.enteredValue)
+  }
 
 }
