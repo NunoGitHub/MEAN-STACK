884b9925ff3657259b70ccab90af7cf0d35a65ce
NunoGitHub <nunoaapp22@hotmail.com>
2024-03-05 16:02:29 +0000

    event binding

--- mean-stack/src/app/posts/post-create/post-create.component.ts
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -1,26 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent{
+export class PostCreateComponent {
+  enteredContent: any = '';
+  enteredTitle: any = '';
+  @Output()
+  postCreated = new EventEmitter();
 
-  newPost:any ="NO CONTENT";
-  enteredValue:any = "";
+  onAddPost() {
+    // alert("Post added!")
+    // console.dir(postInput)
+    //this.newPost= 'The user\'s new post';
+    // this.newPost= postInput.value;
+    const post = { title: this.enteredTitle, content: this.enteredContent };
+    this.postCreated.emit(post);
+    this.enteredContent = '';
+    this.enteredTitle = '';
 
-  onAddPost(postInput:HTMLTextAreaElement){
-   // alert("Post added!")
-   console.dir(postInput)
-   //this.newPost= 'The user\'s new post';
-   this.newPost= postInput.value;
   }
-
-
-  onAddPostEventBinding(){
-    this.newPost= this.enteredValue;
-    alert(this.enteredValue)
-  }
-
 }
